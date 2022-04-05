891e58d9e84c46cd30b765afce5562d5dfc4d6e6
P. ESS <62244934+prdpsingh@users.noreply.github.com>
2022-04-05 21:23:20 +0530

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,1 +1,34 @@
+# read data from kafka topic
+from kafka import KafkaConsumer, TopicPartition
+import logging
+
+topic = "topicname"
+
+bootstrap_server = "yourbootstrapserver:9092"
+sasl_mechanism = "PLAIN"
+username = "username"
+password = "password"
+security_mechanism = "SASL_SSL"
+group_id = "groupid"
+
+consumer=KafkaCOnsumer(topic,
+                      bootstrap_server=bootstrapserver,
+                      auto_offset_reset='earliest',
+                      security_protocol=security_protocol,
+                      group_id=group_id,
+                      sasl_mechanism = sasl_mechanism,
+                      sasl_plain_username = username,
+                      sasl_plain_password = password,
+                      ssl_check_hostname = False,
+                      fetch_max_wait_ms = 5000,
+                      consumer_timeout_ms=60000)
+
+i=0
+
+#print one message
+for msg in consumer:
+  print(msg)
+  i=i+1
+  if(i==1):
+    break
 
